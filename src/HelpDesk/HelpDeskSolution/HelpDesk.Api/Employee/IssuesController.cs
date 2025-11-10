36cec6edfbd2b8f49f15d0431d27b00b3385b7c5
Jeffry Gonzalez <jeff@hypertheory.com>
2025-11-10 14:36:48 -0500

    Added Some Auth

--- src/HelpDesk/HelpDeskSolution/HelpDesk.Api/Employee/IssuesController.cs
+++ src/HelpDesk/HelpDeskSolution/HelpDesk.Api/Employee/IssuesController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HelpDesk.Api.Employee;
 
 public class IssuesController : ControllerBase
 {
+    [Authorize] // requires an authorization header with a bearer JWT or return 401
     [HttpPost("/employee/issues")]
     public async Task<ActionResult> ReportAnIssue()
     {
+        // Transaction List - Fowler
+        // 0. Only employees that are identified by our IDP can do this.
+        //    -- their identity should be in a JWT bearer token in then authorization header.
+        //    -- if they have not been identified, we should send a 401 response (Unauthorized)
+        // 1. look at the data they sent in the body of the request
+        // 2. Validate it - have some rules, enforce them, if it is bad, send them a 400 Bad Request
+        // 3. We have
         return Created();
     }
 }
---
+++ src/HelpDesk/HelpDeskSolution/HelpDesk.Tests/Employee/MustBeAuthenticatedToReportAnIssue.cs
@@ -0,0 +1,21 @@
+﻿
+using HelpDesk.Tests.Fixtures;
+
+namespace HelpDesk.Tests.Employee;
+
+[Collection("AnonymousSystemTestFixture")]
+[Trait("Category", "SystemTest")]
+public class MustBeAuthenticatedToReportAnIssue(AnonymousSystemTestFixture fixture)
+{
+
+    [Fact]
+    public async Task GivesA401ForNoToken()
+    {
+        await fixture.Host.Scenario(api =>
+        {
+            api.Post.Url("/employee/issues");
+            api.StatusCodeShouldBe(401);
+        });
+    }
+    
+}
